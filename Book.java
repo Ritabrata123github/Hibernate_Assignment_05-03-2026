ea296043b343bfa155e029c25f51827b14a77ec1
Ritabrata123github <121775173+Ritabrata123github@users.noreply.github.com>
2026-03-06 18:11:19 +0530

    Update Book.java

--- Book.java
+++ Book.java
@@ -1,71 +1,49 @@
 package com;
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
+
 import javax.persistence.Entity;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Table;
 
 @Entity
-@Table(name= "Customer_Information")
 public class Book {
-		@Id
-		@Column(name= "Customer_Id")
-		@GeneratedValue(strategy= GenerationType.IDENTITY)
-		private int id;
-		@Column(name="Customer_Name",nullable=false)
-		private String name;
-		@Column(name="Customer_Email",nullable=false,unique=true)
-		private String email;
-		@Column(name="Customer_Gender",nullable=false)
-		private String gender;
-		@Column(name="Mobile_Number",nullable=false,unique=true,length=10)
-		private long mobileno;
-		public Book() {
-			
+
+    @Id
+    private int id;
+    private String title;
+    private String author;
+    private double price;
+
+    public Book() {}
+
+    public Book(int id, String title, String author, double price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return "Book [id=" + id + ", title=" + title + ", author=" + author + ", price=" + price + "]";
+    }
+
 }
-	public Book(int id, String name, String email, String gender, long mobileno) {
-			this.id = id;
-			this.name = name;
-			this.email = email;
-			this.gender = gender;
-			this.mobileno = mobileno;
-		}
-
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getGender() {
-		return gender;
-	}
-	public void setGender(String gender) {
-		this.gender = gender;
-	}
-	public long getMobileno() {
-		return mobileno;
-	}
-	public void setMobileno(long mobileno) {
-		this.mobileno = mobileno;
-	}
-	@Override
-	public String toString() {
-		return "UserInformation [id=" + id + ", name=" + name + ", email=" + email + ", gender=" + gender + ", mobileno="
-				+ mobileno + "]";
-	}
-
-	}
